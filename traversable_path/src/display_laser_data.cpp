ae5fcc8a501ede302a50795d2a67ebc0a90bc9ee
fwidmaier <felix.widmaier@student.uni-tuebingen.de>
2012-03-09 14:55:55 +0000

    first attempts in calibrating the laser. launchfile for camera- and laser-viewer
    git-svn-id: https://rarepos.cs.uni-tuebingen.de/svn-path/rabot/trunk/ros/navigation@3620 690284e3-2886-4bbc-99f2-a987503dca67

--- traversable_path/src/display_laser_data.cpp
+++ traversable_path/src/display_laser_data.cpp
@@ -1,31 +1,98 @@
 #include "display_laser_data.h"
 #include <stdio.h>
 #include <algorithm>
+#include <math.h>
 
 using namespace std;
 
-DisplayLaserData::DisplayLaserData()
+DisplayLaserData::DisplayLaserData() :
+        isCalibrated(false)
 {
-    pub = n.advertise<sensor_msgs::LaserScan>("manipulated_scan", 1000);
+    pub = n.advertise<sensor_msgs::LaserScan>("scan/flattend", 1000);
 
     // subscribe laser scanner
     sub = n.subscribe("scan", 1000, &DisplayLaserData::printLaserData, this);
+
+    // register calibration service
+    service = n.advertiseService("calibrate_plane", &DisplayLaserData::calibrate, this);
 }
 
-void DisplayLaserData::printLaserData(const sensor_msgs::LaserScan::Ptr &msg)
+void DisplayLaserData::printLaserData(const sensor_msgs::LaserScanPtr &msg)
 {
+    // if laser is calibrated, subtract plane values
+    if (this->isCalibrated) {
+        for (uint i=0; i < msg->ranges.size(); ++i) {
+            msg->ranges[i] = this->planeRanges[i];
+        }
+    }
+
     // mirror laser data, to make it fit with the camera image
-    reverse(msg->ranges.begin(), msg->ranges.end());
-    reverse(msg->intensities.begin(), msg->intensities.end());
+//    reverse(msg->ranges.begin(), msg->ranges.end());
+//    reverse(msg->intensities.begin(), msg->intensities.end());
 
     // publish modified message
     pub.publish(msg);
 }
 
-void DisplayLaserData::calibrate()
+bool DisplayLaserData::calibrate(std_srvs::Empty::Request& request, std_srvs::Empty::Response& response)
 {
+    ROS_INFO("Use current laser data for calibration.");
 
+    // fetch one laser scan message, which will be used for calibration
+    sensor_msgs::LaserScan scan = *ros::topic::waitForMessage<sensor_msgs::LaserScan>("scan").get();
+
+    //*
+    // calibration by math
+    int mid = scan.ranges.size()/2;
+
+    // simple local search for minimum
+//    cout << "mid: " << mid << endl;
+//    while (msg->ranges[mid] > msg->ranges[mid-10] || msg->ranges[mid] > msg->ranges[mid+10]) {
+//
+//    }
+//    if (msg->ranges[mid] > msg->ranges[mid-10]) {
+//        mid-= 10;
+//    } else {
+//        mid+=10;
+//    }
+//
+//    cout << "min: " << mid << endl << "----------" << endl;
+
+//    int step = 100;
+//    float valMid = msg->ranges[mid];
+//    float valLeft = msg->ranges[mid-step];
+//    float valRight = msg->ranges[mid+step];
+
+    // IDEE: interpoliere messwerte, statt s/cos - s zu verwenden?
+    // Oder interolation nur für minimum-suche? da gibts evtl bessere verfahren?
+
+
+    double s = scan.ranges[mid];
+    for (uint i=0; i < scan.ranges.size(); ++i) {
+        double angle = scan.angle_increment * (i - mid);
+
+        //if (abs(angle) > M_PI/2) {
+        //    // remove values with angle > 90 degree
+        //    scan.ranges[i] = 0;
+        //} else {
+            double d = s/cos(angle) - s;
+            scan.ranges[i] = d;
+        //}
+
+        cout << scan.ranges[i] << endl;
+    }
+
+    //*/
+
+
+    this->planeRanges = scan.ranges;
+    this->isCalibrated = true;
+
+    return true;
 }
+
+
+//--------------------------------------------------------------------------
 
 int main(int argc, char **argv)
 {
--- traversable_path/src/display_laser_data.h
+++ traversable_path/src/display_laser_data.h
@@ -3,6 +3,7 @@
 
 #include "ros/ros.h"
 #include "sensor_msgs/LaserScan.h"
+#include "std_srvs/Empty.h"
 
 class DisplayLaserData
 {
@@ -13,9 +14,12 @@ private:
     ros::NodeHandle n;
     ros::Publisher pub;
     ros::Subscriber sub;
+    ros::ServiceServer service;
+    bool isCalibrated;
+    std::vector<float> planeRanges;
 
-    void printLaserData(const sensor_msgs::LaserScan::Ptr &msg);
-    void calibrate();
+    void printLaserData(const sensor_msgs::LaserScanPtr &msg);
+    bool calibrate(std_srvs::Empty::Request& request, std_srvs::Empty::Response& response);
 };
 
 #endif // DISPLAY_LASER_DATA_H
