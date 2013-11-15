a4620c0fcecdf52b8a2865bcd7e7f836d9c7e031
Felix Widmaier <felix.widmaier@student.uni-tuebingen.de>
2013-11-15 17:46:26 +0100

    Functionallity for sending goals to path_planner and waiting for the result.

--- src/path_control/src/pathcontroller.cpp
+++ src/path_control/src/pathcontroller.cpp
@@ -1,31 +1,56 @@
 #include "pathcontroller.h"
-#include <geometry_msgs/PoseStamped.h>
-#include <nav_msgs/Path.h>
 
 PathController::PathController(ros::NodeHandle &nh):
     node_handle_(nh),
     navigate_to_goal_server_(nh, "navigate_to_goal", false),
-    follow_path_client_("follow_path", false)
+    follow_path_client_("follow_path", false),
+    goal_timestamp_(ros::Time(0))
 {
     //TODO: GoalCallback vs ExecuteCallback?
     //navigate_to_goal_server_.registerGoalCallback(boost::bind(&PathController::NavToGoalActionCallback, this, _1));
     navigate_to_goal_server_.start();
 
     goal_pub_ = nh.advertise<geometry_msgs::PoseStamped>("/goal", 0);
+    path_sub_ = nh.subscribe<nav_msgs::Path>("/path", 10, &PathController::pathCallback, this);
 }
 
-void PathController::NavToGoalActionCallback(const path_msgs::NavigateToGoalActionGoalConstPtr &goal)
+void PathController::navToGoalActionCallback(const path_msgs::NavigateToGoalActionGoalConstPtr &goal)
 {
     // ...
 
-    goal_pub_.publish(goal->goal.goal_pose);
-    //TODO: how to catch the path published by path_planner and ensuring, that the received path is really the one
-    // associated with the current goal? Maybe by using the stamp?
-
-
-    // assume we already have the path
-    nav_msgs::Path path;
+    waitForPath(goal->goal.goal_pose);
 
     path_msgs::FollowPathActionGoal path_action_goal;
-    path_action_goal.goal.path = path;
+    path_action_goal.goal.path = *requested_path_;
+    // ... set some more parameters here...
+
+    //follow_path_client_.sendGoal(path_action_goal, /* ... */);
+
+}
+
+void PathController::pathCallback(const nav_msgs::PathConstPtr &path)
+{
+    //TODO: how to handle unexpected paths? They should be executed but maybe only if the path_follower is idle?
+    // Maybe extra mode for this?
+
+    if (goal_timestamp_.isZero()) {
+        //TODO: what to do here?
+    } else {
+        if (path->header.stamp == goal_timestamp_) {
+            requested_path_ = path;
+            // reset to 0 to signalise, that there is no outstanding path
+            goal_timestamp_ = ros::Time(0);
+        }
+        // else: drop this path (= do nothing)
+    }
+}
+
+void PathController::waitForPath(const geometry_msgs::PoseStamped &goal_pose)
+{
+    //TODO: Can there be concurrency problems? I think not, but better think a bit more deeply about it.
+
+    goal_timestamp_ = goal_pose.header.stamp;
+    goal_pub_.publish(goal_pose);
+
+    while (!goal_timestamp_.isZero());
 }
--- src/path_control/src/pathcontroller.h
+++ src/path_control/src/pathcontroller.h
@@ -6,6 +6,8 @@
 #include <actionlib/client/simple_action_client.h>
 #include <path_msgs/NavigateToGoalAction.h>
 #include <path_msgs/FollowPathAction.h>
+#include <nav_msgs/Path.h>
+#include <geometry_msgs/PoseStamped.h>
 
 class PathController
 {
@@ -29,8 +31,33 @@ private:
     //! Publishes goal as PoseStamped for path_planner and rviz.
     ros::Publisher goal_pub_;
 
-    void NavToGoalActionCallback(const path_msgs::NavigateToGoalActionGoalConstPtr &goal);
+    //! Subscibes for the paths published by path_planner
+    ros::Subscriber path_sub_;
 
+    /**
+     * @brief Timestamp of the last goal that was send to path_planner
+     *
+     * Since the communication between path_control and path_planner is done via simple messages instead of an service
+     * (which is done because goals and paths can then easily be displayed in rviz), there is no nonambiguous
+     * association between the goal, sent to the planner, and the resulting path.
+     * To solve this, the planner sets the timestamp of the path to the one of the goal, so it can be used as a mostly
+     * unique id to associate path with goals. Thus the timestamp of the goal is saved here, so it can be compared with
+     * the timestamps of incomming paths.
+     *
+     * Set this member to Time(0), while there is no outstanding path.
+     */
+    ros::Time goal_timestamp_;
+
+    nav_msgs::PathConstPtr requested_path_;
+
+    void navToGoalActionCallback(const path_msgs::NavigateToGoalActionGoalConstPtr &goal);
+
+    //! Callback for the paths published e.g. by path_planner
+    void pathCallback(const nav_msgs::PathConstPtr &path);
+
+    //! Send a goal pose to path_follower and wait for the resulting path.
+    /** \todo Timeout! */
+    void waitForPath(const geometry_msgs::PoseStamped &goal_pose);
 };
 
 #endif // PATHCONTROLLER_H
