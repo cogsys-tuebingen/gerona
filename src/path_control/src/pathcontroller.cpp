82d776c3748ff33bcb7e85f76e26991f607b6fea
Felix Widmaier <felix.widmaier@student.uni-tuebingen.de>
2013-11-15 16:39:38 +0100

    Added action client for communication between path_control and path_follower

--- src/path_control/src/pathcontroller.cpp
+++ src/path_control/src/pathcontroller.cpp
@@ -1,6 +1,31 @@
 #include "pathcontroller.h"
+#include <geometry_msgs/PoseStamped.h>
+#include <nav_msgs/Path.h>
 
 PathController::PathController(ros::NodeHandle &nh):
-    node_handle_(nh)
+    node_handle_(nh),
+    navigate_to_goal_server_(nh, "navigate_to_goal", false),
+    follow_path_client_("follow_path", false)
 {
+    //TODO: GoalCallback vs ExecuteCallback?
+    //navigate_to_goal_server_.registerGoalCallback(boost::bind(&PathController::NavToGoalActionCallback, this, _1));
+    navigate_to_goal_server_.start();
+
+    goal_pub_ = nh.advertise<geometry_msgs::PoseStamped>("/goal", 0);
+}
+
+void PathController::NavToGoalActionCallback(const path_msgs::NavigateToGoalActionGoalConstPtr &goal)
+{
+    // ...
+
+    goal_pub_.publish(goal->goal.goal_pose);
+    //TODO: how to catch the path published by path_planner and ensuring, that the received path is really the one
+    // associated with the current goal? Maybe by using the stamp?
+
+
+    // assume we already have the path
+    nav_msgs::Path path;
+
+    path_msgs::FollowPathActionGoal path_action_goal;
+    path_action_goal.goal.path = path;
 }
--- src/path_control/src/pathcontroller.h
+++ src/path_control/src/pathcontroller.h
@@ -2,17 +2,35 @@
 #define PATHCONTROLLER_H
 
 #include <ros/ros.h>
+#include <actionlib/server/simple_action_server.h>
+#include <actionlib/client/simple_action_client.h>
+#include <path_msgs/NavigateToGoalAction.h>
+#include <path_msgs/FollowPathAction.h>
 
 class PathController
 {
+
+
 public:
     PathController(ros::NodeHandle &nh);
 
 private:
+    typedef actionlib::SimpleActionServer<path_msgs::NavigateToGoalAction> NavToGoalServer;
+    typedef actionlib::SimpleActionClient<path_msgs::FollowPathAction> FollowPathClient;
+
     ros::NodeHandle node_handle_;
+
+    //! Action server that communicates with the high level control (the node that sends the goal points).
+    NavToGoalServer navigate_to_goal_server_;
+
+    //! Action client to communicate with the path_follower package.
+    FollowPathClient follow_path_client_;
 
     //! Publishes goal as PoseStamped for path_planner and rviz.
     ros::Publisher goal_pub_;
+
+    void NavToGoalActionCallback(const path_msgs::NavigateToGoalActionGoalConstPtr &goal);
+
 };
 
 #endif // PATHCONTROLLER_H
