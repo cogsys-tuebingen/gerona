2a82a6ae3c7f57876ded8b1bb6cb58fa03df45d2
Felix Widmaier <felix.widmaier@student.uni-tuebingen.de>
2014-09-10 17:33:23 +0200

    (Hopefully) better estimation of driving direction for obstacle detection.

--- path_follower/src/coursepredictor.h
+++ path_follower/src/coursepredictor.h
@@ -3,6 +3,7 @@
 
 #include <Eigen/Core>
 #include <ros/ros.h>
+#include <boost/circular_buffer.hpp>
 
 class PathFollower;
 
@@ -24,7 +25,8 @@ public:
      * This prediction is only updated if the robot moved for at least a certain distance and it is smoothed to reduce
      * the effect of jitter in the robots movement.
      *
-     * @return Vector pointing in the direction of movement, relative to robot orientation.
+     * @return Vector pointing in the direction of movement, relative to robot orientation. Zero, if no direction could
+     *         be computed (e.g. at initialization, when there are less than 2 position samples).
      */
     Eigen::Vector2d smoothedDirection();
 
@@ -35,6 +37,9 @@ private:
     PathFollower *path_driver_;
 
     ros::Duration update_intervall_;
+
+
+    boost::circular_buffer<Eigen::Vector2d> last_positions_;
 
     //! Position of the robot (in world frame), when direction was updated the last time.
     Eigen::Vector2d last_position_;
