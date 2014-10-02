6c53c95047302eed0b8a96e2b40b02f84de24656
robot <csrabot@gmail.com>
2014-10-02 18:08:33 +0200

    minor changes

--- src/scan2cloud/scan2cloud.cpp
+++ src/scan2cloud/scan2cloud.cpp
@@ -3,11 +3,11 @@
 ScanConverter::ScanConverter():node_("~"){
     // init parameter with a default value
     node_.param<std::string>("baseFrame",baseFrame_,"/base_link");
-    node_.param<std::string>("scanTopic_front",scanTopic_front_,"/scan/front");
-    node_.param<std::string>("scanTopic_back",scanTopic_back_,"/scan/back");
+    node_.param<std::string>("scanTopic_front",scanTopic_front_,"/scan/front/filtered");
+    node_.param<std::string>("scanTopic_back",scanTopic_back_,"/scan/back/filtered");
     node_.param<std::string>("cloudTopic",cloudTopic_,"/cloud/total");
 
-    node_.param<double>("cloudFilterMean",cloudFilterMean_,51.0);
+    node_.param<double>("cloudFilterMean",cloudFilterMean_,50.0);
     node_.param<double>("cloudFilterStdD",cloudFilterStdD_,1.0);
 
 
