895b692838f7e511825f9b960be866e793e65af1
Goran Huskic <goran.huskic@uni-tuebingen.de>
2016-06-02 18:21:14 +0200

    EKF for ICR estimation - working with bagfiles

--- path_follower/include/path_follower/utils/extended_kalman_filter.h
+++ path_follower/include/path_follower/utils/extended_kalman_filter.h
@@ -1,5 +1,28 @@
 #ifndef EXTENDED_KALMAN_FILTER
 #define EXTENDED_KALMAN_FILTER
 
+#include <Eigen/Core>
+#include <Eigen/Geometry>
+#include <vector>
+#include "std_msgs/Float64MultiArray.h"
+#include "nav_msgs/Odometry.h"
+
+struct EKF {
+    EKF();
+
+    void reset();
+
+    void predict(const std_msgs::Float64MultiArray::ConstPtr& array, double dt);
+
+    void correct(const Eigen::Vector3d &delta);
+
+    Eigen::Matrix<double, 6, 1> x_;
+    Eigen::Matrix<double, 6, 6> F;
+    Eigen::Matrix<double, 6, 6> P;
+
+    Eigen::Matrix<double, 3, 3> R;
+    Eigen::Matrix<double, 6, 6> Q;
+};
+
 #endif // EXTENDED_KALMAN_FILTER
 
