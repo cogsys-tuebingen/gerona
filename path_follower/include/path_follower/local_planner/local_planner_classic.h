79ffb58952a4f045c61d79f8509a83e837a81e53
Luis Gonzalez <lgonzalez@u-172-c036.cs.uni-tuebingen.de>
2016-08-01 19:07:59 +0200

    Out of Range Bug fixed

--- path_follower/include/path_follower/local_planner/local_planner_classic.h
+++ path_follower/include/path_follower/local_planner/local_planner_classic.h
@@ -159,7 +159,7 @@ protected:
     void processPath(NodeT* obj,SubPath& local_wps){
         retrievePath(obj, local_wps);
         last_s = global_path_.s_new();
-        global_path_.set_s_new(local_wps.at(min((int)local_wps.size(),5)).s);
+        global_path_.set_s_new(local_wps.at(min((int)local_wps.size() - 1, 5)).s);
         smoothAndInterpolate(local_wps);
         savePath(local_wps);
     }
