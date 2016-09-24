091705f5217f47ab11d4e3f6f025903036e323e0
buck <sebastian.buck@uni-tuebingen.de>
2016-09-24 15:02:35 +0200

    improved obstacle growth, which can now be specified in the goal message

--- path_control/src/controller_node.cpp
+++ path_control/src/controller_node.cpp
@@ -2,7 +2,7 @@
 #include "pathcontroller.h"
 
 int main(int argc, char** argv) {
-    ros::init(argc, argv, "path_control_node");
+    ros::init(argc, argv, "path_control_node", ros::init_options::NoSigintHandler);
     ros::NodeHandle nh;
 
     PathController pc(nh);
