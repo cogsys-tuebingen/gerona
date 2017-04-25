84a17956c1b37dfeb12cd60c259b95b78d853bdd
Goran Huskic <goran.huskic@uni-tuebingen.de>
2017-04-25 13:24:25 +0200

    ros version 1.11.21 instead of 1.11.0

--- path_follower/src/controller/robotcontroller_dynamic_window.cpp
+++ path_follower/src/controller/robotcontroller_dynamic_window.cpp
@@ -393,7 +393,7 @@ void RobotController_Dynamic_Window::findNextVelocityPair()
     clearing_marker.header.stamp = ros::Time::now();
     clearing_marker.ns = "far_predictions";
     clearing_marker.id = 0;
-#if ROS_VERSION_MINIMUM(1, 11, 0)
+#if ROS_VERSION_MINIMUM(1, 11, 21)
     clearing_marker.action = visualization_msgs::Marker::DELETEALL;
 #else
     clearing_marker.action = 3u;
