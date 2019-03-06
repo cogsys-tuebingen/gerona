8e06d2714b759f5cea0d7b608c7f4b42db8112e9
robot <csrabot@gmail.com>
2019-03-06 17:55:12 +0100

    optimized performance of dt map creator

--- tools/dt_planner/src/poseestimator_dt.cpp
+++ tools/dt_planner/src/poseestimator_dt.cpp
@@ -92,6 +92,84 @@ void PoseEstimatorDT::CreateMap(const cv::Point3f &rPose, const PlannerScorerCon
 
     const short notVisibleLevel = (short)procConfig_.notVisibleLevel;
     const float wheelSupportFarThresholdSqr = wheelSupportFarThreshold*wheelSupportFarThreshold;
+    const float pixelSizeSqr = procConfig_.pixelSize* procConfig_.pixelSize;
+    const float distPowSqr = 1.0f/(scoreConf.heihgtLimitPowPerMeter*scoreConf.heihgtLimitPowPerMeter);
+    float srcVal;
+
+
+    for (unsigned int y = 0; y < orgDem_.rows;++y)
+    {
+        srcPtr = orgDem_.ptr<short>(y);
+        dstPtr = maskImg.ptr<unsigned char>(y);
+        for (unsigned int x = 0; x < orgDem_.cols;++x)
+        {
+            pixelPos.x = (float)x;
+            pixelPos.y = (float)y;
+            diffPos = rPos-pixelPos;
+
+
+            const float distanceSqr = (diffPos.dot(diffPos));
+            const float distM = distanceSqr*pixelSizeSqr;
+            float limitScale = (1.0f + (distM-wheelSupportFarThresholdSqr)*distPowSqr);
+            if (limitScale < 1.0f) limitScale = 1.0f;
+
+            const float lowerLimitF = procConfig_.mapBaseHeight - scoreConf.maxDownStep*procConfig_.heightScale*limitScale;
+            const float upperLimitF = procConfig_.mapBaseHeight + scoreConf.maxUpStep*  procConfig_.heightScale*limitScale;
+
+            srcVal = (float)srcPtr[x];
+            dstPtr[x] = 1;
+
+            if (srcVal < lowerLimitF)
+            {
+                if (!(srcPtr[x] <= notVisibleLevel && (distanceSqr > wheelSupportFarThresholdSqr) ))
+                    dstPtr[x] = 0;
+            }
+            if (srcVal > upperLimitF)
+            {
+                dstPtr[x] = 0;
+            }
+        }
+    }
+
+    cv::Mat res;
+    cv::distanceTransform(maskImg,res,CV_DIST_L2,CV_DIST_MASK_PRECISE);
+
+
+
+
+    dmapPtr_ = CVAlignedMat::Create(res);
+
+    dmap_ = dmapPtr_->mat_;
+
+
+}
+
+/*
+void PoseEstimatorDT::CreateMap(const cv::Point3f &rPose, const PlannerScorerConfigDT& scoreConf )
+{
+    ProcConfigDT procConfig_ = robotModel_.GetProcConfig();
+    //RobotConfigDT robotConfig_ = robotModel_.GetRobotConfig();
+
+    cv::Point2f rPos(rPose.x,rPose.y);
+
+    //short lowerLimit = (short)(procConfig_.mapBaseHeight - scoreConf.maxDownStep*procConfig_.heightScale);
+    //short upperLimit = (short)(procConfig_.mapBaseHeight + scoreConf.maxUpStep*procConfig_.heightScale);
+
+    float wheelSupportFarThreshold = scoreConf.noWheelSupportNearThreshold * procConfig_.pixelSizeInv;
+
+    if (wheelSupportFarThreshold < 0) wheelSupportFarThreshold = 100000;
+
+    cv::Mat maskImg(orgDem_.rows,orgDem_.cols,CV_8U);
+    //dem.copyTo(tDem);
+
+    unsigned char* dstPtr;
+    const short *srcPtr;
+
+    cv::Point2f pixelPos;
+    cv::Point2f diffPos;
+
+    const short notVisibleLevel = (short)procConfig_.notVisibleLevel;
+    const float wheelSupportFarThresholdSqr = wheelSupportFarThreshold*wheelSupportFarThreshold;
 
     float srcVal;
 
@@ -143,8 +221,7 @@ void PoseEstimatorDT::CreateMap(const cv::Point3f &rPose, const PlannerScorerCon
 
 
 }
-
-
+*/
 
 void PoseEstimatorDT::Evaluate(PoseEvalResultsDT &results) const
 {
